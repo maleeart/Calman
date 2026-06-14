2d3318e25de628b07bce0beaf6caf9f9af65b647
maleeart <tuangphetch@gmail.com>
2026-06-14 16:21:10 +0700

    Create food_database.js

---
+++ food_database.js
@@ -0,0 +1,34 @@
+const FOOD_DB = {
+  "7-11": {
+    "อกไก่นุ่ม": { kcal: 110, protein: 23 },
+    "อกไก่พริกไทยดำ": { kcal: 120, protein: 22 },
+    "ไข่ต้ม": { kcal: 70, protein: 6 },
+    "ข้าวกะเพราอกไก่": { kcal: 330, protein: 28 },
+    "ข้าวผัดอกไก่": { kcal: 420, protein: 25 }
+  },
+
+  "Amazon": {
+    "อเมริกาโน่": { kcal: 5, protein: 0 },
+    "ลาเต้เย็น": { kcal: 180, protein: 8 },
+    "คาปูชิโน่เย็น": { kcal: 170, protein: 7 }
+  },
+
+  "Starbucks": {
+    "Americano": { kcal: 10, protein: 0 },
+    "Latte": { kcal: 190, protein: 8 }
+  },
+
+  "KFC": {
+    "ไก่ทอด 1 ชิ้น": { kcal: 250, protein: 18 },
+    "วิงซ์แซ่บ 2 ชิ้น": { kcal: 180, protein: 12 }
+  },
+
+  "MK": {
+    "หมูนุ่ม 1 จาน": { kcal: 180, protein: 18 },
+    "ผักรวม": { kcal: 40, protein: 2 }
+  },
+
+  "Yayoi": {
+    "ข้าวหน้าไก่": { kcal: 550, protein: 30 }
+  }
+};
